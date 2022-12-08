68ce6eb82e15e6199baf31b7e0c423d408813910
VilanyBritto <vilanybritto@gmail.com>
2022-12-07 21:52:17 -0300

    Ajustes no projeto.

--- View/AppModelo.View.Windows/frmPrincipal.Designer.cs
+++ View/AppModelo.View.Windows/frmPrincipal.Designer.cs
@@ -33,12 +33,12 @@
             this.novoFuncionárioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.nacionalidadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.naturalidadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listarCadastradosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaFuncionariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.listarCadastradosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.listaFuncionariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -53,8 +53,8 @@
             this.listarCadastradosToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(937, 39);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(703, 33);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -66,29 +66,45 @@
             this.naturalidadeToolStripMenuItem});
             this.cadastroToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cadastroToolStripMenuItem.Name = "cadastroToolStripMenuItem";
-            this.cadastroToolStripMenuItem.Size = new System.Drawing.Size(126, 35);
+            this.cadastroToolStripMenuItem.Size = new System.Drawing.Size(105, 29);
             this.cadastroToolStripMenuItem.Text = "Cadastrar";
             // 
             // novoFuncionárioToolStripMenuItem
             // 
             this.novoFuncionárioToolStripMenuItem.Name = "novoFuncionárioToolStripMenuItem";
-            this.novoFuncionárioToolStripMenuItem.Size = new System.Drawing.Size(250, 36);
+            this.novoFuncionárioToolStripMenuItem.Size = new System.Drawing.Size(206, 30);
             this.novoFuncionárioToolStripMenuItem.Text = "Funcionário";
             this.novoFuncionárioToolStripMenuItem.Click += new System.EventHandler(this.novoFuncionárioToolStripMenuItem_Click);
             // 
             // nacionalidadeToolStripMenuItem
             // 
             this.nacionalidadeToolStripMenuItem.Name = "nacionalidadeToolStripMenuItem";
-            this.nacionalidadeToolStripMenuItem.Size = new System.Drawing.Size(250, 36);
+            this.nacionalidadeToolStripMenuItem.Size = new System.Drawing.Size(206, 30);
             this.nacionalidadeToolStripMenuItem.Text = "Nacionalidade";
             this.nacionalidadeToolStripMenuItem.Click += new System.EventHandler(this.nacionalidadeToolStripMenuItem_Click);
             // 
             // naturalidadeToolStripMenuItem
             // 
             this.naturalidadeToolStripMenuItem.Name = "naturalidadeToolStripMenuItem";
-            this.naturalidadeToolStripMenuItem.Size = new System.Drawing.Size(250, 36);
+            this.naturalidadeToolStripMenuItem.Size = new System.Drawing.Size(206, 30);
             this.naturalidadeToolStripMenuItem.Text = "Naturalidade";
             this.naturalidadeToolStripMenuItem.Click += new System.EventHandler(this.naturalidadeToolStripMenuItem_Click);
+            // 
+            // listarCadastradosToolStripMenuItem
+            // 
+            this.listarCadastradosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listaFuncionariosToolStripMenuItem});
+            this.listarCadastradosToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listarCadastradosToolStripMenuItem.Name = "listarCadastradosToolStripMenuItem";
+            this.listarCadastradosToolStripMenuItem.Size = new System.Drawing.Size(178, 29);
+            this.listarCadastradosToolStripMenuItem.Text = "Listar Cadastrados";
+            // 
+            // listaFuncionariosToolStripMenuItem
+            // 
+            this.listaFuncionariosToolStripMenuItem.Name = "listaFuncionariosToolStripMenuItem";
+            this.listaFuncionariosToolStripMenuItem.Size = new System.Drawing.Size(261, 30);
+            this.listaFuncionariosToolStripMenuItem.Text = "Lista de Funcionários";
+            this.listaFuncionariosToolStripMenuItem.Click += new System.EventHandler(this.listaFuncionariosToolStripMenuItem_Click);
             // 
             // panel1
             // 
@@ -96,10 +112,10 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 39);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Location = new System.Drawing.Point(0, 33);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(248, 384);
+            this.panel1.Size = new System.Drawing.Size(186, 311);
             this.panel1.TabIndex = 3;
             // 
             // label1
@@ -107,9 +123,10 @@
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(0, 219);
+            this.label1.Location = new System.Drawing.Point(0, 177);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 25);
+            this.label1.Size = new System.Drawing.Size(113, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "     Vilany Britto";
             // 
@@ -118,10 +135,10 @@
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.pictureBox1.Image = global::AppModelo.View.Windows.Properties.Resources.zyro_image__1_c;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 244);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 197);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(248, 140);
+            this.pictureBox1.Size = new System.Drawing.Size(186, 114);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -130,38 +147,22 @@
             this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.SystemColors.ControlText;
             this.dateTimePicker1.CalendarTitleForeColor = System.Drawing.SystemColors.Window;
             this.dateTimePicker1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(248, 401);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePicker1.Location = new System.Drawing.Point(186, 324);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(689, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(517, 20);
             this.dateTimePicker1.TabIndex = 9;
-            // 
-            // listarCadastradosToolStripMenuItem
-            // 
-            this.listarCadastradosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.listaFuncionariosToolStripMenuItem});
-            this.listarCadastradosToolStripMenuItem.Name = "listarCadastradosToolStripMenuItem";
-            this.listarCadastradosToolStripMenuItem.Size = new System.Drawing.Size(144, 35);
-            this.listarCadastradosToolStripMenuItem.Text = "Listar Cadastrados";
-            // 
-            // listaFuncionariosToolStripMenuItem
-            // 
-            this.listaFuncionariosToolStripMenuItem.Name = "listaFuncionariosToolStripMenuItem";
-            this.listaFuncionariosToolStripMenuItem.Size = new System.Drawing.Size(230, 26);
-            this.listaFuncionariosToolStripMenuItem.Text = "Lista de Funcionários";
-            this.listaFuncionariosToolStripMenuItem.Click += new System.EventHandler(this.listaFuncionariosToolStripMenuItem_Click);
             // 
             // frmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(937, 423);
+            this.ClientSize = new System.Drawing.Size(703, 344);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
